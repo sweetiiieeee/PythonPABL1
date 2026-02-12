6d94f114e8db1c9fbec70beb6a1db798b15c7000
Sweety Singh <sweety.kumari.cs28@iilm.edu>
2026-02-12 20:29:24 +0530

    Create 2D_MATRIX.py

---
+++ week 3 /2D_MATRIX.py
@@ -0,0 +1,26 @@
+class Solution:
+    def searchMatrix(self, matrix, target):
+        if not matrix or not matrix[0]:
+            return False
+
+        m = len(matrix)
+        n = len(matrix[0])
+
+        low = 0
+        high = m * n - 1
+
+        while low <= high:
+            mid = (low + high) // 2
+
+            # convert 1D index to 2D index
+            row = mid // n
+            col = mid % n
+
+            if matrix[row][col] == target:
+                return True
+            elif matrix[row][col] < target:
+                low = mid + 1
+            else:
+                high = mid - 1
+
+        return False
